839a8814eef9edb5a01f63da351e5a07b1525ecd
rowince <rowincerock@gmail.com>
2022-05-21 18:57:34 +0530

    new odd fun added

--- test_tut2.py
+++ test_tut2.py
@@ -1,8 +1,16 @@
 from fun.tut2 import evenfun
+from fun.tut2 import oddfun
 
 
 def test_evenfun():
     inputt = [10, 4, 3, 5, 7, 2, 12]
     res = evenfun(inputt)
     out = [10, 4, 2, 12]
+    assert res == out
+
+
+def test_oddfun():
+    inputt = [10, 4, 3, 5, 7, 2, 12]
+    res = oddfun(inputt)
+    out = [3, 5, 7]
     assert res == out
